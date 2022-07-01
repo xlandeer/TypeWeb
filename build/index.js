442f2cc2c782e20941b921c1603f72f9cdb0b484
alexander <Alexander-joerges@t-online.de>
2022-07-01 20:27:27 +0200

    Added Build

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function changeColor(color) {
+    let elem = document.getElementById("para1");
+    if (elem !== null) {
+        elem.style.color = color;
+    }
+}
+//# sourceMappingURL=index.js.map
