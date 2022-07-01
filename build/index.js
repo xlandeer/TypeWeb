52ecd6557ba0db461080ffe9f1d56238cd2c69f7
Yannick Wehner <wehner@camalot.de>
2022-07-01 22:35:55 +0200

    2235220701

--- build/index.js
+++ build/index.js
@@ -1,8 +1,20 @@
 "use strict";
-function changeColor(color) {
-    let elem = document.getElementById("para1");
-    if (elem !== null) {
-        elem.style.color = color;
+class ListNode {
+    constructor(text) {
+        var _a;
+        this.text = text;
+        this.parent = document.querySelector(".checklist-inner");
+        this.node = document.createElement("div");
+        this.node.textContent = text;
+        this.node.className = 'node';
+        (_a = this.parent) === null || _a === void 0 ? void 0 : _a.appendChild(this.node);
     }
 }
+let addBtn = document.querySelector('.add-btn');
+addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener('click', () => {
+    const input = document.querySelector('.checklist .checklist-controls input');
+    if (input === null || input === void 0 ? void 0 : input.value) {
+        let node = new ListNode(input.value);
+    }
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-function changeColor(color: string) {
-  let elem = document.getElementById("para1");
-  if(elem !== null) {
-    elem.style.color = color
+class ListNode {
+  parent
+  node
+  constructor(
+    private text: string
+  ) {
+    this.parent = document.querySelector(".checklist-inner")
+    this.node = document.createElement("div")
+    this.node.textContent = text
+    this.node.className = 'node'
+    this.parent?.appendChild(this.node)
   }
 }
+
+
+let addBtn = document.querySelector('.add-btn')
+addBtn?.addEventListener('click', () => {
+  const input = document.querySelector('.checklist .checklist-controls input') as HTMLInputElement | null
+  if (input?.value) {
+    let node = new ListNode(input.value)
+  }
+})
