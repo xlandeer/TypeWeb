931a0c6a099be67dfde85476ecfa37452bcb82b7
alexander <Alexander-joerges@t-online.de>
2022-07-02 18:23:18 +0200

    Usage of setAttribute and added svg image

--- build/index.js
+++ build/index.js
@@ -4,6 +4,7 @@ class ListNode {
         this.text = text;
         this.parent = document.querySelector(".checklist-inner");
         this.nodeWrapper = document.createElement("div");
+<<<<<<< Updated upstream
         this.nodeWrapper.className = 'node-wrapper';
         let nodeElements = this.createNodeElements();
         appendElements(this.nodeWrapper, nodeElements.tick, nodeElements.node, nodeElements.deleteBtn);
@@ -21,14 +22,43 @@ class ListNode {
         return { tick, node, deleteBtn };
     }
     ///TODO: addElement function (Overloads)
+<<<<<<< HEAD
+=======
+    addElements(...elementNames) {
+        let ret = [];
+        for (const elementName of elementNames) {
+            ret.push(document.createElement(elementName));
+=======
+        this.nodeWrapper.className = "node-wrapper";
+        let nodeElements = this.createNodeElements();
+        ListNode.appendElements(this.nodeWrapper, nodeElements.tick, nodeElements.node, nodeElements.deleteBtn);
+        if (this.parent) {
+            ListNode.appendElements(this.parent, this.nodeWrapper);
+        }
+        nodeElements.deleteBtn.addEventListener("click", () => this.nodeWrapper.remove());
+    }
+    createNodeElements() {
+        let tick = this.addElement("input", ["class", "tick"], ["type", "checkbox"]);
+        let node = this.addElement("div", ["class", "node"]);
+        let deleteBtn = this.addElement("input", ["class", "delete-btn"], ["type", "image"], ["src", "icons/x_btn.svg"]);
+        node.textContent = this.text;
+        return { tick, node, deleteBtn };
+    }
+    // TODO: addElement function HTML Hardcode overload
+>>>>>>> 38c3fc3 (Usage of setAttribute and added svg image)
     // Backend Save function of Data
     addElement(name, ...attributes) {
         let element = document.createElement(name);
         for (const attribute of attributes) {
             element.setAttribute(attribute[0], attribute[1]);
+<<<<<<< HEAD
+=======
+>>>>>>> Stashed changes
+>>>>>>> 38c3fc3 (Usage of setAttribute and added svg image)
         }
         return element;
     }
+<<<<<<< Updated upstream
 }
 function appendElements(parent, ...nodes) {
     for (const node of nodes) {
@@ -40,13 +70,32 @@ addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener('click',
     const input = document.querySelector('.checklist .checklist-controls input');
     if (input === null || input === void 0 ? void 0 : input.value) {
         let node = new ListNode(input.value);
+<<<<<<< HEAD
         input.value = '';
+=======
+=======
+    static createNewListNode() {
+        const input = document.querySelector(".checklist .checklist-controls input");
+        if (input === null || input === void 0 ? void 0 : input.value) {
+            let node = new ListNode(input.value);
+            input.value = "";
+        }
+    }
+    ;
+    static appendElements(parent, ...nodes) {
+        for (const node of nodes) {
+            parent.appendChild(node);
+        }
+    }
+}
+let addBtn = document.querySelector(".add-btn");
+let ms2Enter = document.getElementById("textField");
+ms2Enter === null || ms2Enter === void 0 ? void 0 : ms2Enter.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
+        ListNode.createNewListNode();
+>>>>>>> Stashed changes
+>>>>>>> 38c3fc3 (Usage of setAttribute and added svg image)
     }
 });
-/*
-<div class="node">
-  <div class="check">haken</div>
-  <div class="node-text">Text</div>
-</div>
-*/ 
+addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener("click", ListNode.createNewListNode);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 
 class ListNode {
-  private parent
-  private nodeWrapper
+  private parent;
+  private nodeWrapper;
 
+<<<<<<< Updated upstream
   constructor(
     private text: string,
   ) {
@@ -15,6 +16,7 @@ class ListNode {
     if (this.parent) {
       appendElements(this.parent, this.nodeWrapper)
     }
+<<<<<<< HEAD
     nodeElements.deleteBtn.addEventListener('click', () => this.nodeWrapper.remove())
   }
 
@@ -24,14 +26,52 @@ class ListNode {
     let deleteBtn = this.addElement("div",["classname","delete-btn"])
     console.log(deleteBtn)
     return{tick,node,deleteBtn}
+=======
+    deleteBtn.addEventListener('click', () => this.delete(this.nodeWrapper))
+=======
+  constructor(private text: string) {
+    this.parent = document.querySelector(".checklist-inner");
+    this.nodeWrapper = document.createElement("div");
+    this.nodeWrapper.className = "node-wrapper";
+    let nodeElements = this.createNodeElements();
+    ListNode.appendElements(
+      this.nodeWrapper,
+      nodeElements.tick,
+      nodeElements.node,
+      nodeElements.deleteBtn
+    );
+    if (this.parent) {
+      ListNode.appendElements(this.parent, this.nodeWrapper);
+    }
+    nodeElements.deleteBtn.addEventListener("click", () =>
+      this.nodeWrapper.remove()
+    );
+>>>>>>> 38c3fc3 (Usage of setAttribute and added svg image)
   }
-  ///TODO: addElement function (Overloads)
+
+  private createNodeElements() {
+    let tick = this.addElement("input",["class", "tick"],["type", "checkbox"]);
+    let node = this.addElement("div", ["class", "node"]);
+    let deleteBtn = this.addElement("input", ["class", "delete-btn"], ["type", "image"],["src", "icons/x_btn.svg"]);
+    node.textContent = this.text;
+    return { tick, node, deleteBtn };
+>>>>>>> Stashed changes
+  }
+  // TODO: addElement function HTML Hardcode overload
   // Backend Save function of Data
+<<<<<<< HEAD
 
   private addElement(name: string,...attributes: [string,string][]) {
     let element = document.createElement(name)
     for (const attribute of attributes) {
       element.setAttribute(attribute[0],attribute[1])
+=======
+<<<<<<< Updated upstream
+  private addElements(...elementNames: string[]) {
+    let ret: Element[] = [];
+    for (const elementName of elementNames) {
+      ret.push(document.createElement(elementName));
+>>>>>>> 38c3fc3 (Usage of setAttribute and added svg image)
     }
     return element
   }
@@ -53,4 +93,50 @@ addBtn?.addEventListener('click', () => {
     let node = new ListNode(input.value)
     input.value = ''
   }
+<<<<<<< HEAD
 })
+=======
+})
+
+/*
+<div class="node">
+  <div class="check">haken</div>
+  <div class="node-text">Text</div>
+</div>
+*/
+=======
+
+  private addElement(name: string, ...attributes: [string, string][]) {
+    let element = document.createElement(name);
+    for (const attribute of attributes) {
+      element.setAttribute(attribute[0], attribute[1]);
+    }
+    return element;
+  }
+  public static createNewListNode() {
+    const input: HTMLInputElement | null = document.querySelector(
+      ".checklist .checklist-controls input"
+    );
+    if (input?.value) {
+      let node = new ListNode(input.value);
+      input.value = "";
+    }
+  };
+  public static appendElements<T extends HTMLElement>(parent: Element, ...nodes: T[]) {
+    for (const node of nodes) {
+      parent.appendChild(node);
+    }
+  }
+}
+
+
+let addBtn = document.querySelector(".add-btn");
+let ms2Enter = document.getElementById("textField");
+ms2Enter?.addEventListener("keypress", (e: KeyboardEvent) => {
+  if (e.key == "Enter") {
+    ListNode.createNewListNode();
+  }
+});
+addBtn?.addEventListener("click", ListNode.createNewListNode);
+>>>>>>> Stashed changes
+>>>>>>> 38c3fc3 (Usage of setAttribute and added svg image)
