46e59650fc725d75e199b2d46acde5df60d897bb
alexander <Alexander-joerges@t-online.de>
2022-07-17 21:15:58 +0200

    Functioning Storage

--- build/index.js
+++ build/index.js
@@ -1,5 +1,27 @@
 "use strict";
 var _a, _b;
+class IngredientMap {
+    constructor(map = {}) {
+        this.map = map;
+    }
+    *generateIterator() {
+        for (const ingredientKey in this.map) {
+            yield { key: ingredientKey, value: this.map[ingredientKey] };
+        }
+    }
+    set(key, value) {
+        this.map[key] = value;
+    }
+    get(key) {
+        return this.map[key];
+    }
+    print() {
+        let iterator = this.generateIterator();
+        for (const ingredient of iterator) {
+            console.log(ingredient.key + ": " + ingredient.value);
+        }
+    }
+}
 class Cocktail {
     constructor(name, ingredients, imgPath, parent) {
         this.name = name;
@@ -10,25 +32,36 @@ class Cocktail {
         this.addRepresentation();
     }
     addRepresentation() {
-        let cocktailStructure = '';
-        cocktailStructure = '<div class="cocktail col-12 col-md-6 col-lg-4 my-4">';
-        cocktailStructure += '<div class="cocktail-wrapper">';
-        cocktailStructure += '<div class="cocktail-img">';
-        cocktailStructure += `<img src="${this.imgPath}">`;
-        cocktailStructure += '</div>';
-        cocktailStructure += '<div class="cocktail-content">';
-        cocktailStructure += `<h2>${this.name}</h2>`;
-        cocktailStructure += '<ul class="ingredients-list">###INGREDIENTS###</ul>';
-        cocktailStructure += '</div>';
-        cocktailStructure += '</div>';
-        cocktailStructure += '</div>';
-        let ingredientsStructure = '';
-        for (const ingredrient of this.ingredients) {
-            ingredientsStructure += `<li class="ingredient">${ingredrient[0]}: <span>${ingredrient[1]}</span></li>`;
+        let cocktailWrapper = document.createElement('div');
+        cocktailWrapper.setAttribute('class', 'cocktail-wrapper');
+        let image = document.createElement('img');
+        image.src = this.imgPath;
+        let nameLabel = document.createElement('h2');
+        nameLabel.textContent = this.name;
+        let ingredients = document.createElement('ul');
+        let iterator = this.ingredients.generateIterator();
+        for (const ingredrient of iterator) {
+            let ingredient = document.createElement('li');
+            ingredient.textContent = ingredrient.key + ': ' + ingredrient.value;
+            ingredients.appendChild(ingredient);
         }
-        console.log(cocktailStructure.replace('###INGREDIENTS###', ingredientsStructure));
-        cocktailStructure = cocktailStructure.replace('###INGREDIENTS###', ingredientsStructure);
-        this.parent.innerHTML = cocktailStructure;
+        cocktailWrapper.appendChild(image);
+        cocktailWrapper.appendChild(nameLabel);
+        cocktailWrapper.appendChild(ingredients);
+        this.parent.appendChild(cocktailWrapper);
+    }
+    static loadFromStorage() {
+        for (const key in Object.assign({}, localStorage)) {
+            let objectStr = localStorage.getItem(key);
+            if (objectStr) {
+                let cocktailObject = JSON.parse(objectStr);
+                let ingredients = new IngredientMap(cocktailObject.ingredients.map);
+                let cocktail = new Cocktail(cocktailObject.name, ingredients, cocktailObject.imgPath, parentDOMElement);
+            }
+        }
+    }
+    getIngredients() {
+        return this.ingredients;
     }
 }
 Cocktail.amount = 0;
@@ -38,7 +71,7 @@ const inputIngrName = document.querySelector('.input-wrapper .add-ingr-name');
 const inputIngrAmt = document.querySelector('.input-wrapper .add-ingr-amt');
 const parentDOMElement = document.querySelector('.cocktail-wrapper');
 const ingredientWrapper = document.querySelector('.input-wrapper .ingredient-wrapper');
-let ingredients = new Map();
+let ingredients = new IngredientMap();
 (_a = document.querySelector('.input-wrapper .add-ingr-btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
     if (inputIngrName.value && /^[0-9]+$/g.test(inputIngrAmt.value)) {
         // store ingredient in map
@@ -53,10 +86,14 @@ let ingredients = new Map();
 (_b = document.querySelector('.input-wrapper .add-cocktail-btn')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => {
     if (cocktailName.value && /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm.test(cocktailPicture.value)) {
         const newCocktail = new Cocktail(cocktailName.value, ingredients, cocktailPicture.value, parentDOMElement);
-        ingredients.clear();
+        localStorage.setItem('C' + newCocktail.id, JSON.stringify(newCocktail));
+        ingredients = new IngredientMap();
         cocktailName.value = '';
         cocktailPicture.value = '';
         ingredientWrapper.innerHTML = '';
     }
+});
+document.addEventListener('DOMContentLoaded', () => {
+    Cocktail.loadFromStorage();
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,83 @@
-class Cocktail{
+class IngredientMap {
+    constructor(private map: { [key: string]: number } = {}) {
+
+    }
+
+    *generateIterator() {
+        for (const ingredientKey in this.map) {
+            yield { key: ingredientKey, value: this.map[ingredientKey] };
+        }
+    }
+
+    public set(key: string, value: number) {
+        this.map[key] = value;
+    }
+    public get(key: string) {
+        return this.map[key];
+    }
+
+    public print() {
+        let iterator = this.generateIterator();
+        for (const ingredient of iterator) {
+            console.log(ingredient.key + ": " + ingredient.value);
+
+        }
+    }
+
+
+}
+
+
+
+
+class Cocktail {
     static amount: number = 0;
     id: number;
-    constructor(private name: string, private ingredients: Map<string, number>, private imgPath: string, private parent: HTMLDivElement) {
+    constructor(private name: string, private ingredients: IngredientMap, private imgPath: string, private parent: HTMLDivElement) {
         this.id = ++Cocktail.amount;
         this.addRepresentation();
     }
 
     addRepresentation() {
-        let cocktailStructure: string = '';
-        cocktailStructure   = '<div class="cocktail col-12 col-md-6 col-lg-4 my-4">';
-        cocktailStructure       += '<div class="cocktail-wrapper">';
-        cocktailStructure           += '<div class="cocktail-img">';
-        cocktailStructure               += `<img src="${this.imgPath}">`;
-        cocktailStructure           += '</div>';
-        cocktailStructure           += '<div class="cocktail-content">';
-        cocktailStructure               += `<h2>${this.name}</h2>`;
-        cocktailStructure               += '<ul class="ingredients-list">###INGREDIENTS###</ul>';
-        cocktailStructure           += '</div>';
-        cocktailStructure       += '</div>';
-        cocktailStructure   += '</div>';
-
-        let ingredientsStructure: string = '';
-        for (const ingredrient of this.ingredients) {
-            ingredientsStructure += `<li class="ingredient">${ingredrient[0]}: <span>${ingredrient[1]}</span></li>`;
-        }       
-        cocktailStructure = cocktailStructure.replace('###INGREDIENTS###', ingredientsStructure);
-        this.parent.innerHTML  = cocktailStructure;
+        let cocktailWrapper = document.createElement('div');
+        cocktailWrapper.setAttribute('class', 'cocktail-wrapper');
+        let image = document.createElement('img');
+        image.src = this.imgPath;
+        let nameLabel = document.createElement('h2');
+        nameLabel.textContent = this.name;
+        let ingredients = document.createElement('ul');
+        let iterator = this.ingredients.generateIterator();
+        for (const ingredrient of iterator) {
+            let ingredient = document.createElement('li');
+            ingredient.textContent = ingredrient.key + ': ' + ingredrient.value;
+            ingredients.appendChild(ingredient);
+        }
+        cocktailWrapper.appendChild(image);
+        cocktailWrapper.appendChild(nameLabel);
+        cocktailWrapper.appendChild(ingredients);
+        this.parent.appendChild(cocktailWrapper);
     }
-    // addIngredient(name: string, value: number) {
-    //     this.ingredients.set(name,value);
-    // }
+
+    static loadFromStorage() {
+        for (const key in { ...localStorage }) {
+            let objectStr = localStorage.getItem(key);
+            if (objectStr) {
+
+                let cocktailObject = JSON.parse(objectStr);
+
+                let ingredients = new IngredientMap(cocktailObject.ingredients.map);
+
+                let cocktail = new Cocktail(cocktailObject.name, ingredients, cocktailObject.imgPath, parentDOMElement);
+
+            }
+        }
+
+
+    }
+
+    public getIngredients() {
+        return this.ingredients;
+    }
 }
 
 const cocktailName = document.querySelector('.input-wrapper .name') as HTMLInputElement;
@@ -39,11 +87,12 @@ const inputIngrAmt = document.querySelector('.input-wrapper .add-ingr-amt') as H
 
 const parentDOMElement = document.querySelector('.cocktail-wrapper') as HTMLDivElement;
 const ingredientWrapper = document.querySelector('.input-wrapper .ingredient-wrapper') as HTMLDivElement;
-let ingredients: Map<string, number> = new Map();
+let ingredients = new IngredientMap();
 
 document.querySelector('.input-wrapper .add-ingr-btn')?.addEventListener('click', () => {
     if (inputIngrName.value && /^[0-9]+$/g.test(inputIngrAmt.value)) {
         // store ingredient in map
+
         ingredients.set(inputIngrName.value, parseInt(inputIngrAmt.value));
         // output ingredient to the user
         ingredientWrapper.innerHTML += `<div>${inputIngrName.value}: ${inputIngrAmt.value}</div>`;
@@ -56,9 +105,16 @@ document.querySelector('.input-wrapper .add-ingr-btn')?.addEventListener('click'
 document.querySelector('.input-wrapper .add-cocktail-btn')?.addEventListener('click', () => {
     if (cocktailName.value && /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm.test(cocktailPicture.value)) {
         const newCocktail: Cocktail = new Cocktail(cocktailName.value, ingredients, cocktailPicture.value, parentDOMElement);
-        ingredients.clear();
+        localStorage.setItem('C' + newCocktail.id, JSON.stringify(newCocktail));
+
+        ingredients = new IngredientMap();
         cocktailName.value = '';
         cocktailPicture.value = '';
         ingredientWrapper.innerHTML = '';
+
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    Cocktail.loadFromStorage();
+})
