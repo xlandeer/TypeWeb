db1738ac1c5a11ee83cbadbab83285560187c080
alexander <Alexander-joerges@t-online.de>
2022-07-21 02:33:47 +0200

    Cleaned Up Code - added Utils namespace

--- build/index.js
+++ build/index.js
@@ -1,5 +1,22 @@
 "use strict";
 var _a, _b;
+var Utils;
+(function (Utils) {
+    function appendElements(parent, ...nodes) {
+        for (const node of nodes) {
+            parent.appendChild(node);
+        }
+    }
+    Utils.appendElements = appendElements;
+    function createElementWithAttributes(name, ...attributes) {
+        let element = document.createElement(name);
+        for (const attribute of attributes) {
+            element.setAttribute(attribute[0], attribute[1]);
+        }
+        return element;
+    }
+    Utils.createElementWithAttributes = createElementWithAttributes;
+})(Utils || (Utils = {}));
 class IngredientMap {
     constructor(map = {}) {
         this.map = map;
@@ -31,45 +48,44 @@ class Cocktail {
         this.id = id;
     }
     addRepresentation() {
-        let cocktailWrapper = document.createElement('div');
-        // cocktailWrapper.setAttribute('data-id', this.id?.toString());
-        cocktailWrapper.setAttribute('class', 'cocktail-wrapper');
-        let image = document.createElement('img');
-        image.src = this.imgPath;
-        let nameLabel = document.createElement('h2');
+        //Create every Element with Attributes
+        let cocktailWrapper = Utils.createElementWithAttributes("div", ["class", "cocktail-wrapper"]);
+        let image = Utils.createElementWithAttributes("img", ["src", this.imgPath]);
+        let nameLabel = Utils.createElementWithAttributes("h2");
         nameLabel.textContent = this.name;
-        let ingredients = document.createElement('ul');
-        let deleteBtn = document.createElement('input');
-        deleteBtn.setAttribute("type", "image");
-        deleteBtn.setAttribute("src", "images/x_btn.svg");
-        let iterator = this.ingredients.generateIterator();
-        for (const ingredrient of iterator) {
-            let ingredient = document.createElement('li');
-            ingredient.textContent = ingredrient.key + ': ' + ingredrient.value.amt + ' ' + ingredrient.value.measure;
-            ingredients.appendChild(ingredient);
-        }
+        let deleteBtn = Utils.createElementWithAttributes("input", ["type", "image"], ["src", "images/x_btn.svg"]);
+        //Add DeleteBtn Event Listener
         const copy = this;
-        deleteBtn.addEventListener('click', () => {
+        deleteBtn.addEventListener("click", () => {
             Cocktail.deleteFromStorage(copy);
         });
-        cocktailWrapper.appendChild(image);
-        cocktailWrapper.appendChild(nameLabel);
-        cocktailWrapper.appendChild(ingredients);
-        cocktailWrapper.appendChild(deleteBtn);
-        this.parent.appendChild(cocktailWrapper);
+        //generate Ingredientlist
+        let ingredients = document.createElement("ul");
+        let iterator = this.ingredients.generateIterator();
+        for (const ingredrient of iterator) {
+            let ingredient = document.createElement("li");
+            ingredient.textContent = ingredrient.key + ": " + ingredrient.value.amt + " " + ingredrient.value.measure;
+            ingredients.appendChild(ingredient);
+        }
+        //Append Elements to their corresponding Parent
+        Utils.appendElements(cocktailWrapper, image, nameLabel, ingredients, deleteBtn);
+        Utils.appendElements(this.parent, cocktailWrapper);
     }
-    static loadFromStorage(searchFilter = '') {
+    static loadFromStorage(searchFilter = "") {
         $.ajax({
-            url: 'index.php',
-            type: 'GET',
+            url: "index.php",
+            type: "GET",
             data: { searchFilter: searchFilter },
             success: function (returnData) {
-                parentDOMElement.innerHTML = '';
+                parentDOMElement.innerHTML = "";
                 if (returnData) {
                     for (const element of JSON.parse(returnData)) {
                         let newIngredients = new IngredientMap();
                         for (const ingr of element.ingredients) {
-                            newIngredients.set(ingr.ingr_name, { amt: parseInt(ingr.ingr_amt), measure: ingr.ingr_measure });
+                            newIngredients.set(ingr.ingr_name, {
+                                amt: parseInt(ingr.ingr_amt),
+                                measure: ingr.ingr_measure,
+                            });
                         }
                         const newCocktail = new Cocktail(element.name, newIngredients, element.imageUrl, parentDOMElement, element.id);
                         newCocktail.addRepresentation();
@@ -77,20 +93,20 @@ class Cocktail {
                 }
             },
             error: function (xhr, status, error) {
-                let errorMessage = xhr.status + ': ' + xhr.statusText;
-                console.log('Error - ' + errorMessage);
-            }
+                let errorMessage = xhr.status + ": " + xhr.statusText;
+                console.log("Error - " + errorMessage);
+            },
         });
     }
     static deleteFromStorage(cocktail) {
         $.ajax({
-            url: 'index.php',
-            type: 'POST',
+            url: "index.php",
+            type: "POST",
             // all data || notation in JSON
             data: { intention: "delete", id: cocktail.id },
             success: function (data, status, xhr) {
                 Cocktail.loadFromStorage();
-            }
+            },
         });
     }
     static saveToStorage(cocktail) {
@@ -100,55 +116,66 @@ class Cocktail {
         //     data: {name: cocktail.name, imageUrl: cocktail.imgPath}
         // }).catch(function(response) {
         //     console.log(response);
-        // });  
+        // });
         $.ajax({
-            url: 'index.php',
-            type: 'POST',
+            url: "index.php",
+            type: "POST",
             // all data || notation in JSON
-            data: { intention: "save", name: cocktail.name, imageUrl: cocktail.imgPath, ingredients: cocktail.ingredients },
+            data: {
+                intention: "save",
+                name: cocktail.name,
+                imageUrl: cocktail.imgPath,
+                ingredients: cocktail.ingredients,
+            },
             success: function (data, status, xhr) {
                 Cocktail.loadFromStorage();
-            }
+            },
         });
     }
     getIngredients() {
         return this.ingredients;
     }
 }
-const cocktailName = document.querySelector('.input-wrapper .name');
-const cocktailPicture = document.querySelector('.input-wrapper #photo');
-const inputIngrName = document.querySelector('.input-wrapper .add-ingr-name');
-const inputIngrAmt = document.querySelector('.input-wrapper .add-ingr-amt');
-const selectIngrMeasure = document.querySelector('.input-wrapper .meas-select');
-const parentDOMElement = document.querySelector('.cocktail-section');
-const ingredientWrapper = document.querySelector('.input-wrapper .ingredient-wrapper');
+const cocktailName = document.querySelector(".input-wrapper .name");
+const cocktailPicture = document.querySelector(".input-wrapper #photo");
+const inputIngrName = document.querySelector(".input-wrapper .add-ingr-name");
+const inputIngrAmt = document.querySelector(".input-wrapper .add-ingr-amt");
+const selectIngrMeasure = document.querySelector(".input-wrapper .meas-select");
+const parentDOMElement = document.querySelector(".cocktail-section");
+const ingredientWrapper = document.querySelector(".input-wrapper .ingredient-wrapper");
 let ingredients = new IngredientMap();
-const cocktailFilter = document.querySelector('.search-wrapper .cocktail-filter');
-cocktailFilter.addEventListener('input', (event) => {
+const cocktailFilter = document.querySelector(".search-wrapper .cocktail-filter");
+cocktailFilter.addEventListener("input", (event) => {
     Cocktail.loadFromStorage(cocktailFilter.value);
 });
-(_a = document.querySelector('.input-wrapper .add-ingr-btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
-    if (inputIngrName.value && /^[0-9]+$/g.test(inputIngrAmt.value) && selectIngrMeasure.value) {
+(_a = document.querySelector(".input-wrapper .add-ingr-btn")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+    if (inputIngrName.value &&
+        /^[0-9]+$/g.test(inputIngrAmt.value) &&
+        selectIngrMeasure.value) {
         // store ingredient in map
-        ingredients.set(inputIngrName.value, { amt: parseInt(inputIngrAmt.value), measure: selectIngrMeasure.value });
+        ingredients.set(inputIngrName.value, {
+            amt: parseInt(inputIngrAmt.value),
+            measure: selectIngrMeasure.value,
+        });
         // output ingredient to the user
         ingredientWrapper.innerHTML += `<div>${inputIngrName.value}: ${inputIngrAmt.value} ${selectIngrMeasure.value}</div>`;
         // reset the input fields
-        inputIngrName.value = '';
-        inputIngrAmt.value = '';
+        inputIngrName.value = "";
+        inputIngrAmt.value = "";
     }
 });
-(_b = document.querySelector('.input-wrapper .add-cocktail-btn')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => {
-    if (cocktailName.value && /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm.test(cocktailPicture.value)) {
+(_b = document.querySelector(".input-wrapper .add-cocktail-btn")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", () => {
+    if (cocktailName.value &&
+        /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm.test(cocktailPicture.value)) {
         const newCocktail = new Cocktail(cocktailName.value, ingredients, cocktailPicture.value, parentDOMElement);
         Cocktail.saveToStorage(newCocktail);
         ingredients = new IngredientMap();
-        cocktailName.value = '';
-        cocktailPicture.value = '';
-        ingredientWrapper.innerHTML = '';
+        cocktailName.value = "";
+        cocktailPicture.value = "";
+        ingredientWrapper.innerHTML = "";
     }
 });
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     Cocktail.loadFromStorage();
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,183 +1,236 @@
-type Amount = {amt: number, measure: string}
-
+type Amount = { amt: number; measure: string };
+namespace Utils {
+  export function appendElements(
+    parent: Element,
+    ...nodes: HTMLElement[]
+  ) {
+    for (const node of nodes) {
+      parent.appendChild(node);
+    }
+  }
+  export function createElementWithAttributes(name: string, ...attributes: [string, string][]) {
+    let element = document.createElement(name);
+    for (const attribute of attributes) {
+      element.setAttribute(attribute[0], attribute[1]);
+    }
+    return element;
+  }
+}
 
 class IngredientMap {
+  constructor(private map: { [key: string]: Amount } = {}) {}
 
-
-    constructor(private map: { [key: string]: Amount } = {}) {
-
+  *generateIterator() {
+    for (const ingredientKey in this.map) {
+      yield { key: ingredientKey, value: this.map[ingredientKey] };
     }
+  }
 
-    *generateIterator() {
-        for (const ingredientKey in this.map) {
-            yield { key: ingredientKey, value: this.map[ingredientKey] };
-        }
+  public set(key: string, value: Amount) {
+    this.map[key] = value;
+  }
+  public get(key: string) {
+    return this.map[key];
+  }
+
+  public print() {
+    let iterator = this.generateIterator();
+    for (const ingredient of iterator) {
+      console.log(ingredient.key + ": " + ingredient.value);
     }
-
-    public set(key: string, value: Amount) {
-        this.map[key] = value;
-    }
-    public get(key: string) {
-        return this.map[key];
-    }
-
-    public print() {
-        let iterator = this.generateIterator();
-        for (const ingredient of iterator) {
-            console.log(ingredient.key + ": " + ingredient.value);
-
-        }
-    }
-
-
+  }
 }
-
-
-
 
 class Cocktail {
-    constructor(private name: string, private ingredients: IngredientMap, private imgPath: string, private parent: HTMLDivElement , private id?: number) {
-        
-    }
+  constructor(
+    private name: string,
+    private ingredients: IngredientMap,
+    private imgPath: string,
+    private parent: HTMLDivElement,
+    private id?: number
+  ) {}
 
-    addRepresentation() {
-        let cocktailWrapper = document.createElement('div');
-        // cocktailWrapper.setAttribute('data-id', this.id?.toString());
-        cocktailWrapper.setAttribute('class', 'cocktail-wrapper');
-        let image = document.createElement('img');
-        image.src = this.imgPath;
-        let nameLabel = document.createElement('h2');
-        nameLabel.textContent = this.name;
-        let ingredients = document.createElement('ul');
-        let deleteBtn = document.createElement('input');
-        deleteBtn.setAttribute("type","image");
-        deleteBtn.setAttribute("src","images/x_btn.svg");
-        let iterator = this.ingredients.generateIterator();
-        for (const ingredrient of iterator) {
-            let ingredient = document.createElement('li');
-            ingredient.textContent = ingredrient.key + ': ' + ingredrient.value.amt + ' ' + ingredrient.value.measure;
-            ingredients.appendChild(ingredient);
+  addRepresentation() {
+    //Create every Element with Attributes
+    let cocktailWrapper = Utils.createElementWithAttributes("div",["class","cocktail-wrapper"]);
+    let image = Utils.createElementWithAttributes("img",["src",this.imgPath]);
+    let nameLabel = Utils.createElementWithAttributes("h2");nameLabel.textContent = this.name;
+    let deleteBtn = Utils.createElementWithAttributes("input",["type", "image"],["src", "images/x_btn.svg"]);
+
+    //Add DeleteBtn Event Listener
+    const copy = this;
+    deleteBtn.addEventListener("click", () => {
+      Cocktail.deleteFromStorage(copy);
+    });
+
+    //generate Ingredientlist
+    let ingredients = document.createElement("ul");
+    let iterator = this.ingredients.generateIterator();
+    for (const ingredrient of iterator) {
+      let ingredient = document.createElement("li");
+      ingredient.textContent = ingredrient.key + ": " + ingredrient.value.amt + " " + ingredrient.value.measure;
+      ingredients.appendChild(ingredient);
+    }
+    
+    //Append Elements to their corresponding Parent
+    Utils.appendElements(cocktailWrapper,image,nameLabel,ingredients,deleteBtn);
+    Utils.appendElements(this.parent,cocktailWrapper);
+  }
+
+  static loadFromStorage(searchFilter: string = "") {
+    $.ajax({
+      url: "index.php",
+      type: "GET",
+      data: { searchFilter: searchFilter },
+      success: function (returnData) {
+        parentDOMElement.innerHTML = "";
+        if (returnData) {
+          for (const element of JSON.parse(returnData)) {
+            let newIngredients = new IngredientMap();
+            for (const ingr of element.ingredients) {
+              newIngredients.set(ingr.ingr_name, {
+                amt: parseInt(ingr.ingr_amt),
+                measure: ingr.ingr_measure,
+              });
+            }
+            const newCocktail: Cocktail = new Cocktail(
+              element.name,
+              newIngredients as IngredientMap,
+              element.imageUrl,
+              parentDOMElement,
+              element.id
+            );
+            newCocktail.addRepresentation();
+          }
         }
-        const copy = this;
-        deleteBtn.addEventListener('click',() => {
-            Cocktail.deleteFromStorage(copy);
-        });
-        cocktailWrapper.appendChild(image);
-        cocktailWrapper.appendChild(nameLabel);
-        cocktailWrapper.appendChild(ingredients);
-        cocktailWrapper.appendChild(deleteBtn);
-        this.parent.appendChild(cocktailWrapper);
-    }
+      },
+      error: function (xhr, status, error) {
+        let errorMessage = xhr.status + ": " + xhr.statusText;
+        console.log("Error - " + errorMessage);
+      },
+    });
+  }
 
-    static loadFromStorage(searchFilter: string = '') {
-        $.ajax({
-            url: 'index.php',
-            type: 'GET',
-            data: {searchFilter: searchFilter},
-            success: function (returnData) {
-                parentDOMElement.innerHTML = '';
-                if(returnData) {
-                    for (const element of JSON.parse(returnData)) {
-                        let newIngredients = new IngredientMap();
-                        for (const ingr of element.ingredients) {
-                            newIngredients.set(ingr.ingr_name, {amt: parseInt(ingr.ingr_amt), measure: ingr.ingr_measure});
-                        }
-                        const newCocktail: Cocktail = new Cocktail(element.name, newIngredients as IngredientMap, element.imageUrl, parentDOMElement, element.id);                    
-                        newCocktail.addRepresentation();
-                    }  
-                }         
-                
-            },
-            error: function (xhr, status, error) {
-                let errorMessage = xhr.status + ': ' + xhr.statusText;
-                console.log('Error - ' + errorMessage);
-            }
-        });
+  static deleteFromStorage(cocktail: Cocktail) {
+    $.ajax({
+      url: "index.php", // url where the data should be sent
+      type: "POST", // http method
 
-    }
+      // all data || notation in JSON
+      data: { intention: "delete", id: cocktail.id },
+      success: function (data, status, xhr) {
+        Cocktail.loadFromStorage();
+      },
+    });
+  }
 
-    static deleteFromStorage(cocktail: Cocktail) {
-        $.ajax({
-            url: 'index.php',// url where the data should be sent
-            type: 'POST',  // http method
+  static saveToStorage(cocktail: Cocktail) {
+    // $.ajax({
+    //     method: "POST",
+    //     url: "index.php",
+    //     data: {name: cocktail.name, imageUrl: cocktail.imgPath}
+    // }).catch(function(response) {
+    //     console.log(response);
+    // });
+    $.ajax({
+      url: "index.php", // url where the data should be sent
+      type: "POST", // http method
 
-            // all data || notation in JSON
-            data: { intention: "delete", id: cocktail.id},
-            success: function (data, status, xhr) {
-                Cocktail.loadFromStorage();
-            }
-        });
-    }
+      // all data || notation in JSON
+      data: {
+        intention: "save",
+        name: cocktail.name,
+        imageUrl: cocktail.imgPath,
+        ingredients: cocktail.ingredients,
+      },
+      success: function (data, status, xhr) {
+        Cocktail.loadFromStorage();
+      },
+    });
+  }
 
-    static saveToStorage(cocktail: Cocktail) {
-		// $.ajax({
-        //     method: "POST",
-        //     url: "index.php",
-        //     data: {name: cocktail.name, imageUrl: cocktail.imgPath}
-        // }).catch(function(response) {
-        //     console.log(response);
-        // });  
-        $.ajax({
-            url: 'index.php',// url where the data should be sent
-            type: 'POST',  // http method
-
-            // all data || notation in JSON
-            data: { intention: "save", name: cocktail.name, imageUrl: cocktail.imgPath, ingredients:cocktail.ingredients },
-            success: function (data, status, xhr) {
-                Cocktail.loadFromStorage();
-            }
-        });
-    }
-
-    public getIngredients() {
-        return this.ingredients;
-    }
+  public getIngredients() {
+    return this.ingredients;
+  }
 }
 
-const cocktailName = document.querySelector('.input-wrapper .name') as HTMLInputElement;
-const cocktailPicture = document.querySelector('.input-wrapper #photo') as HTMLInputElement;
-const inputIngrName = document.querySelector('.input-wrapper .add-ingr-name') as HTMLInputElement;
-const inputIngrAmt = document.querySelector('.input-wrapper .add-ingr-amt') as HTMLInputElement;
-const selectIngrMeasure = document.querySelector('.input-wrapper .meas-select') as HTMLSelectElement;
+const cocktailName = document.querySelector(
+  ".input-wrapper .name"
+) as HTMLInputElement;
+const cocktailPicture = document.querySelector(
+  ".input-wrapper #photo"
+) as HTMLInputElement;
+const inputIngrName = document.querySelector(
+  ".input-wrapper .add-ingr-name"
+) as HTMLInputElement;
+const inputIngrAmt = document.querySelector(
+  ".input-wrapper .add-ingr-amt"
+) as HTMLInputElement;
+const selectIngrMeasure = document.querySelector(
+  ".input-wrapper .meas-select"
+) as HTMLSelectElement;
 
-const parentDOMElement = document.querySelector('.cocktail-section') as HTMLDivElement;
-const ingredientWrapper = document.querySelector('.input-wrapper .ingredient-wrapper') as HTMLDivElement;
+const parentDOMElement = document.querySelector(
+  ".cocktail-section"
+) as HTMLDivElement;
+const ingredientWrapper = document.querySelector(
+  ".input-wrapper .ingredient-wrapper"
+) as HTMLDivElement;
+
 let ingredients = new IngredientMap();
 
-const cocktailFilter = document.querySelector('.search-wrapper .cocktail-filter') as HTMLInputElement;
+const cocktailFilter = document.querySelector(
+  ".search-wrapper .cocktail-filter"
+) as HTMLInputElement;
 
-cocktailFilter.addEventListener('input', (event: any) => { 
-    Cocktail.loadFromStorage(cocktailFilter.value);
-
-})
-
-
-document.querySelector('.input-wrapper .add-ingr-btn')?.addEventListener('click', () => {
-    if (inputIngrName.value && /^[0-9]+$/g.test(inputIngrAmt.value) && selectIngrMeasure.value) {
-        // store ingredient in map
-
-        ingredients.set(inputIngrName.value, {amt: parseInt(inputIngrAmt.value),measure: selectIngrMeasure.value} );
-        // output ingredient to the user
-        ingredientWrapper.innerHTML += `<div>${inputIngrName.value}: ${inputIngrAmt.value} ${selectIngrMeasure.value}</div>`;
-        // reset the input fields
-        inputIngrName.value = '';
-        inputIngrAmt.value = '';
-    }
+cocktailFilter.addEventListener("input", (event: any) => {
+  Cocktail.loadFromStorage(cocktailFilter.value);
 });
 
-document.querySelector('.input-wrapper .add-cocktail-btn')?.addEventListener('click', () => {
-    if (cocktailName.value && /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm.test(cocktailPicture.value)) {
-        const newCocktail: Cocktail = new Cocktail(cocktailName.value, ingredients, cocktailPicture.value, parentDOMElement);
+document.querySelector(".input-wrapper .add-ingr-btn")
+  ?.addEventListener("click", () => {
+    if (
+      inputIngrName.value &&
+      /^[0-9]+$/g.test(inputIngrAmt.value) &&
+      selectIngrMeasure.value
+    ) {
+      // store ingredient in map
 
-        Cocktail.saveToStorage(newCocktail);
-        ingredients = new IngredientMap();
-        cocktailName.value = '';
-        cocktailPicture.value = '';
-        ingredientWrapper.innerHTML = '';
-
+      ingredients.set(inputIngrName.value, {
+        amt: parseInt(inputIngrAmt.value),
+        measure: selectIngrMeasure.value,
+      });
+      // output ingredient to the user
+      ingredientWrapper.innerHTML += `<div>${inputIngrName.value}: ${inputIngrAmt.value} ${selectIngrMeasure.value}</div>`;
+      // reset the input fields
+      inputIngrName.value = "";
+      inputIngrAmt.value = "";
     }
-});
+  });
 
-document.addEventListener('DOMContentLoaded', () => {
-    Cocktail.loadFromStorage();
-})
+document.querySelector(".input-wrapper .add-cocktail-btn")
+  ?.addEventListener("click", () => {
+    if (
+      cocktailName.value &&
+      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm.test(
+        cocktailPicture.value
+      )
+    ) {
+      const newCocktail: Cocktail = new Cocktail(
+        cocktailName.value,
+        ingredients,
+        cocktailPicture.value,
+        parentDOMElement
+      );
+
+      Cocktail.saveToStorage(newCocktail);
+      ingredients = new IngredientMap();
+      cocktailName.value = "";
+      cocktailPicture.value = "";
+      ingredientWrapper.innerHTML = "";
+    }
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  Cocktail.loadFromStorage();
+});
